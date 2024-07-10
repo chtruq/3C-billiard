adbfa142a4c0abcd4ee99ed9f6ab58d7bee21ac7
chtruq <trunghcw@gmail.com>
2024-07-10 13:30:49 +0700

    splash screen

--- app/index.jsx
+++ app/index.jsx
@@ -13,27 +13,36 @@ export default function App() {
   const bg = require("../assets/background.png");
 
   useFocusEffect(() => {
-    setTimeout(() => {
-      router.replace("/signin");
-    }, 1000);
+    SplashScreen.preventAutoHideAsync()
+      .then((result) => {
+        if (result) {
+          setTimeout(() => {
+            SplashScreen.hideAsync();
+          }, 2000);
+        }
+      })
+      .catch((error) => console.error(error));
+
+    router.replace("/signin");
   });
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <ImageBackground
-        source={bg}
-        resizeMode="contain"
-        className="w-[100vw] h-[100vh]"
-      >
-        <View className="flex-1 items-center justify-center">
-          <Image source={require("../assets/3C-Icon.png")} />
-          <Text className="text-lg mt-2 font-pregular text-black">
-            Connect-Cue-Community
-          </Text>
-          {/* <ActivityIndicator className="mt-4" size="large" /> */}
-        </View>
-      </ImageBackground>
-      <StatusBar style="auto" />
-    </View>
+    // <View className="flex-1 items-center justify-center">
+    //   <ImageBackground
+    //     source={bg}
+    //     resizeMode="contain"
+    //     className="w-[100vw] h-[100vh]"
+    //   >
+    //     <View className="flex-1 items-center justify-center">
+    //       <Image source={require("../assets/3C-Icon.png")} />
+    //       <Text className="text-lg mt-2 font-pregular text-black">
+    //         Connect-Cue-Community
+    //       </Text>
+    //       {/* <ActivityIndicator className="mt-4" size="large" /> */}
+    //     </View>
+    //   </ImageBackground>
+    //   <StatusBar style="auto" />
+    // </View>
+    <></>
   );
 }
