21af7e93da65c0c74ef67765afc8e2652e3ea967
chtruq <trunghcw@gmail.com>
2024-07-11 11:48:18 +0700

    route fix

--- app/index.jsx
+++ app/index.jsx
@@ -12,6 +12,7 @@ import {
   ImageBackground,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { useGlobalContext } from "../context/GlobalProvider";
@@ -24,25 +25,71 @@ export default function index() {
 
   if (!loading && isLogged) return <Redirect href="/home" />;
 
-  if (!loading && !isLogged) return <Redirect href="/signin" />;
-  useEffect(() => {
-    if (!loading && isLogged) {
-      SplashScreen.hideAsync();
-      router.push("/home");
-    }
-    if (!loading && !isLogged) {
-      SplashScreen.hideAsync();
-      router.push("/signin");
-    }
-  }, [loading, isLogged]);
+  // if (!loading && !isLogged) return <Redirect href="/signin" />;
+  // useEffect(() => {
+  //   if (!loading && isLogged) {
+  //     SplashScreen.hideAsync();
+  //     router.push("/home");
+  //   }
+  //   if (!loading && !isLogged) {
+  //     SplashScreen.hideAsync();
+  //     router.push("/signin");
+  //   }
+  // }, [loading, isLogged]);
 
   return (
     <View className="flex-1 items-center justify-center">
       <ImageBackground
-        source={bg}
+        source={require("../assets/background.png")}
         resizeMode="contain"
-        className="w-[100vw] h-[100vh]"
-      ></ImageBackground>
+        className="w-[100vw] h-[100vh] "
+      >
+        <View className="flex-1 items-center">
+          <Image
+            className="mt-[20%] w-[120px] h-[120px]"
+            source={require("../assets/3C-Icon.png")}
+          />
+        </View>
+
+        <View className="flex-1 bg-white rounded-3xl">
+          <View className="m-8">
+            <View>
+              <Text className="mt-3 text-3xl font-pbold text-blue-950">
+                Chào mừng đến với
+              </Text>
+              <Text className="mt-3 text-4xl font-pbold text-primary">
+                3C Billiard
+              </Text>
+              <Text className="text-gray-500 mt-4">Connect-Cue-Community</Text>
+            </View>
+
+            <View className="w-[84vw] pt-[15vh] ">
+              <View className="justify-around flex-row rounded-full border-2">
+                <TouchableOpacity
+                  onPress={() => {
+                    router.push("/signin");
+                  }}
+                  className="bg-primary rounded-full w-[50%] justify-center py-4 px-10"
+                >
+                  <Text className=" text-center font-psemibold text-white">
+                    Đăng nhập
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {
+                    router.push("/signup");
+                  }}
+                  className="w-[50%] justify-center py-4 px-10 "
+                >
+                  <Text className="text-center text-primary font-semibold">
+                    Đăng kí
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
